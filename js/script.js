75a0b20ca2d127387d7bf5b2fadba51b2aba580d
TobiaRigon <rigontobia@gmail.com>
2023-11-21 14:00:11 +0100

    FizzBuzz

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// crea ciclo i da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // crea valore
+    let output = "";
+
+    // se divisibile sia per 3 che per 5 valore = FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        output = "FizzBuzz";
+    } else if (i % 5 === 0) {
+        // se divisibile per 5 valore = Buzz
+        output = "Buzz";
+    } else if (i % 3 === 0) {
+        // se divisibile per 3 valore = Fizz
+        output = "Fizz";
+    } else {
+        // altrimenti valore = i
+        output = i;
+    }
+
+    // stampa valore in console
+    console.log(output);
+}
